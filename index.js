9bec667555fab59e01dcea624917df0d677fbf90
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-04-03 23:51:53 -0700

    Use let instead of var

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ let plotData = (teams, max, median, count, labels) => {
 let findMedian = (values) => {
     values.sort((a,b) => { return a - b; });
 
-    var half = Math.floor(values.length / 2);
+    let half = Math.floor(values.length / 2);
 
     if (values.length % 2) return values[half];
     else return (values[half - 1] + values[half]) / 2.0;
